61d5aa8898f1bb573bdca458d4cfd08584148cc4
itguytrades <itguytrades@proton.me>
2024-10-12 22:47:54 -0700

    added swap functionality

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -16,7 +16,11 @@ import {
 
 import {
   setContract,
-  sharesLoaded
+  sharesLoaded,
+  swapsLoaded,
+  swapRequest,
+  swapSuccess,
+  swapFail
 } from './reducers/amm'
 
 import TOKEN_ABI from '../abis/Token.json';
@@ -78,4 +82,34 @@ export const loadBalances = async (amm, tokens, account, dispatch) => {
 
   const shares = await amm.shares(account)
   dispatch(sharesLoaded(ethers.utils.formatUnits(shares.toString(), 'ether')))
+}
+
+// ------------------------------------------------------------------------------
+// SWAP
+
+export const swap = async (provider, amm, token, symbol, amount, dispatch) => {
+  try {
+
+    dispatch(swapRequest())
+
+    let transaction
+
+    const signer = await provider.getSigner()
+
+    transaction = await token.connect(signer).approve(amm.address, amount)
+    await transaction.wait()
+
+    if (symbol === "DAPP") {
+      transaction = await amm.connect(signer).swapToken1(amount)
+    } else {
+      transaction = await amm.connect(signer).swapToken2(amount)
+    }
+
+    await transaction.wait()
+
+    dispatch(swapSuccess(transaction.hash))
+
+  } catch (error) {
+    dispatch(swapFail())
+  }
 }
--- src/store/reducers/amm.js
+++ src/store/reducers/amm.js
@@ -5,7 +5,22 @@ export const amm = createSlice({
   initialState: {
     contract: null,
     shares: 0,
-    swaps: []
+    swaps: [],
+    depositing: {
+      isDepositing: false,
+      isSucccess: false,
+      transactionHash: null
+    },
+    withdrawing: {
+      isWithdrawing: false,
+      isSucccess: false,
+      transactionHash: null
+    },
+    swapping: {
+      isSwapping: false,
+      isSucccess: false,
+      transactionHash: null
+    }
   },
   reducers: {
     setContract: (state, action) => {
@@ -13,14 +28,35 @@ export const amm = createSlice({
     },
     sharesLoaded: (state, action) => {
       state.shares = action.payload
+    },
+    swapsLoaded: (state, action) => {
+      state.swaps = action.payload
+    },
+    swapRequest: (state, action) => {
+      state.swapping.isSwapping = true
+      state.swapping.isSuccess = false
+      state.swapping.transactionHash = null
+    },
+    swapSuccess: (state, action) => {
+      state.swapping.isSwapping = false
+      state.swapping.isSuccess = true
+      state.swapping.transactionHash = action.payload
+    },
+    swapFail: (state, action) => {
+      state.swapping.isSwapping = false
+      state.swapping.isSuccess = false
+      state.swapping.transactionHash = null
     }
-
   }
 })
 
 export const {
   setContract,
-  sharesLoaded
+  sharesLoaded,
+  swapsLoaded,
+  swapRequest,
+  swapSuccess,
+  swapFail
 } = amm.actions;
 
 export default amm.reducer;
